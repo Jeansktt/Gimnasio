03513bbbe2a920d43c7e9695f57b9e9a6cb53785
Jeansktt <105426487+Jeansktt@users.noreply.github.com>
2025-05-24 21:42:58 +0200

    empezando reservas1

---
+++ back-proyecto/controllers/getReservas.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require_once("../db/conexion.php");
+header("Content-Type: application/json");
+
+class ObtenerReservas {
+    public static function verReservas() {
+        $con = conectar();
+
+        try {
+            $reservas = self::selectAllReservas($con);
+
+            echo json_encode([
+                'status' => 'Success',
+                'data' => [
+                    'reservas' => $reservas
+                ]
+            ]);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode([
+                'status' => 'Error',
+                'message' => 'Hubo un problema al obtener las reservas: ' . $e->getMessage()
+            ]);
+        }
+    }
+
+    public static function selectAllReservas($mysqli) {
+        $stmt = $mysqli->prepare("SELECT * FROM usuarios_clases");
+
+        if (!$stmt) {
+            throw new Exception("Error en la consulta: " . $mysqli->error);
+        }
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $reservas = [];
+        while ($row = $result->fetch_assoc()) {
+            $reservas[] = $row;
+        }
+
+        return $reservas;
+    }
+}
+?>
--- back-proyecto/routes/ver_reservas.php
+++ back-proyecto/routes/ver_reservas.php
@@ -1,9 +1,8 @@
 <?php 
-require_once("../controllers/deleteUser.php");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST");
 header('Content-Type: application/json');
-
-borrarusuarios::eliminarusuarios();
+require_once("../controllers/getReservas.php");
+ObtenerReservas::verReservas();
 ?>
